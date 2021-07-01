2ca3e75f3e6fe5f29a8c8ad12d9b0dac2d76acec
moskalevNick <moskalevnikolay1@mail.ru>
2021-07-01 17:42:29 +0300

    remove http only for cookies

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000'  //https://myperfecttodo.web.app  http://localhost:3000  
+    origin: 'https://myperfecttodo.web.app'  //https://myperfecttodo.web.app  http://localhost:3000  
 }));
 app.use('/api', router);
 app.use(errorMiddleware);
