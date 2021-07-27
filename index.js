35fbce870d1f3836ff5ed9bab95cdafb800dfd22
moskalevNick <moskalevnikolay1@mail.ru>
2021-07-27 17:16:35 +0300

    https

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express');
+const https = require('https')
 const cors = require('cors');
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose');
@@ -9,6 +10,7 @@ const errorMiddleware = require('./middlewares/error-middleware');
 const PORT = process.env.PORT || 5000;
 const app = express()
 
+app.use(https())
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
