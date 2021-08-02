a55c77d54f782a0b135b96018044da64f7b0bc9e
moskalevNick <moskalevnikolay1@mail.ru>
2021-08-02 16:24:30 +0300

    fix bugs

--- router/index.js
+++ router/index.js
@@ -8,12 +8,12 @@ const authMiddleware = require('../middlewares/auth-middleware');
 router.post('/registration',
     body('email').isEmail(),
     body('password').isLength({min: 3, max: 32}),
-    body('name').isLength({min: 3, max: 32}),
+    body('name').isLength({min: 2, max: 32}),
     userController.registration
 );
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
-router.get('/activate/:link', userController.activate);
+router.get('/activate/:link', authMiddleware, userController.activate);
 router.get('/refresh', userController.refresh);
 router.post('/city/:id', userController.setCity);
 router.get('/todos', authMiddleware, todosController.getTodos);
