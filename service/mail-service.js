c86db0c416fb1fc8bb4dae4cbe3a58557499097e
moskalevNick <moskalevnikolay1@mail.ru>
2021-08-03 11:01:58 +0300

    redact mailer

--- service/mail-service.js
+++ service/mail-service.js
@@ -28,7 +28,7 @@ class MailService {
                     <div>
                         <h3> Thank you for choosing Task manager </h3>
                         <h1> Please confirm your email. We don't spam </h1>
-                        <button href="${link}">${link}</button>
+                        <button href="${link}">confirm</button>
                     </div>
                 `
         })
